3d3bf101f22378a89ac69850059b9a8d4009b0ce
SaloniSharma <salonisharma_1987@yahoo.co.in>
2016-11-26 01:11:47 +1100

    "variable name changes"

--- app/src/main/java/com/weatherapp/constants/URLConstants.java
+++ app/src/main/java/com/weatherapp/constants/URLConstants.java
@@ -4,6 +4,6 @@ package com.weatherapp.constants;
  * Created by Saloni on 21/10/16.
  */
 public class URLConstants {
-    public static final String abc = "https://api.darksky.net/forecast/";
+    public static final String BASE_URL = "https://api.darksky.net/forecast/";
     public static final String API_KEY = "a570f5bdb48ee833562cf27fc4cbc4fc/";
 }
